1d3c72c45c0b02781dc043e1bc1ad615bd4348c6
seunghwan <sun1238799@naver.com>
2022-11-04 02:29:44 +0900

    fix : Add zustand arrayFunction

--- client/src/page/Movie.tsx
+++ client/src/page/Movie.tsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import myStore from "../store";
 const Movie = () => {
-  return <div>안녕 난 무비야</div>;
+  const { todo, countInput, count, increaseCount } = myStore();
+  return (
+    <div>
+      <h1>안녕 난 무비야{count}</h1>
+      <button onClick={increaseCount}>버튼</button>
+      <div>
+        {todo?.map((item) => {
+          return <h1>{item}</h1>;
+        })}
+      </div>
+      <button onClick={() => countInput(5)}>버튼</button>
+    </div>
+  );
 };
 
 export default Movie;
--- client/src/store.ts
+++ client/src/store.ts
@@ -1,11 +1,39 @@
+import { BrowserRouter } from "react-router-dom";
 import create from "zustand";
 
-const count = create((set) => ({
+interface CountStore {
+  todo: number[];
+  count: number;
+  increaseCount: () => void;
+  countInput: (input: number) => void;
+}
+
+const myStore = create<CountStore>((set) => ({
+  todo: [],
   count: 0,
   increaseCount: () => {
-    set((state) => {
-      console.log(state);
-      return state.count + 1;
-    });
+    set((state) => ({ count: state.count + 1 }));
+  },
+  countInput: (input) => {
+    set((state) => ({ todo: [...state.todo, input] }));
   },
 }));
+
+export default myStore;
+// import create from "zustand";
+
+// interface BearState {
+//   bears: number;
+//   count: number;
+//   increase: (by: number) => void;
+// }
+
+// const myStore = create<BearState>()((set) => ({
+//   bears: 0,
+//   count: 0,
+//   increase: (
+//     by // state == 지금 현재의 나의 상태
+//   ) => set((state) => ({ bears: state.bears + by, count: state.count + by })),
+// }));
+
+// export default myStore;
